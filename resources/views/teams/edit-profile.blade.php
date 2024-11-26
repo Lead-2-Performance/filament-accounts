06400109fbabf52e7d019203d338caee62eaf9a3
Belal Hamdan <belal.hamdan25@gmail.com>
2024-11-26 17:13:59 +0300

    fix browserSessionsForm

--- resources/views/teams/edit-profile.blade.php
+++ resources/views/teams/edit-profile.blade.php
@@ -2,30 +2,24 @@
     <x-filament-panels::form wire:submit="updateProfile">
         {{ $this->editProfileForm }}
 
-        <x-filament-panels::form.actions
-            alignment="right"
-            :actions="$this->getUpdateProfileFormActions()"
-        />
+        <x-filament-panels::form.actions alignment="right" :actions="$this->getUpdateProfileFormActions()" />
     </x-filament-panels::form>
 
-    @if(filament()->getPlugin('filament-saas-accounts')->editPassword)
+    @if (filament()->getPlugin('filament-saas-accounts')->editPassword)
         <x-filament-panels::form wire:submit="updatePassword">
             {{ $this->editPasswordForm }}
 
-            <x-filament-panels::form.actions
-                alignment="right"
-                :actions="$this->getUpdatePasswordFormActions()"
-            />
+            <x-filament-panels::form.actions alignment="right" :actions="$this->getUpdatePasswordFormActions()" />
         </x-filament-panels::form>
     @endif
 
-    @if(filament()->getPlugin('filament-saas-accounts')->browserSesstionManager)
+    @if (filament()->getPlugin('filament-saas-accounts')->browserSessionsForm)
         <x-filament-panels::form>
             {{ $this->browserSessionsForm }}
         </x-filament-panels::form>
     @endif
 
-    @if(filament()->getPlugin('filament-saas-accounts')->deleteAccount)
+    @if (filament()->getPlugin('filament-saas-accounts')->deleteAccount)
         <x-filament-panels::form>
             {{ $this->deleteAccountForm }}
         </x-filament-panels::form>
